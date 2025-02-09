c59a5c5b5dd65e10f857a9739e26ad9b527a4d54
Krishna <neupane.krishna33@gmail.com>
2025-02-09 15:11:28 +0545

    added

--- config/site.tsx
+++ config/site.tsx
@@ -71,12 +71,12 @@ export const siteConfig = {
 
   // certification
   certification: [
-    {
-      title: 'AWS Knowledge: Architecting',
-      issuedby: 'Amazon Web Services Training and Certification- Coursera',
-      imageURL: 'https://images.credly.com/size/680x680/images/519a6dba-f145-4c1a-85a2-1d173d6898d9/image.png',
-      link: 'https://www.credly.com/badges/3030abd1-c596-4f7d-a4d1-d590c9720e3d/public_url',
-      },
+    // {
+    //   title: 'AWS Knowledge: Architecting',
+    //   issuedby: 'Amazon Web Services Training and Certification- Coursera',
+    //   imageURL: 'https://images.credly.com/size/680x680/images/519a6dba-f145-4c1a-85a2-1d173d6898d9/image.png',
+    //   link: 'https://www.credly.com/badges/3030abd1-c596-4f7d-a4d1-d590c9720e3d/public_url',
+    //   },
     // {
     //   title: "Okta Certified Professional",
     //   issueby: "Okta",
