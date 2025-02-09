c253767b527818fc2b8d730785181f7145150ec4
Krishna <neupane.krishna33@gmail.com>
2025-02-09 15:52:15 +0545

    added

--- app/components/profile/education-list.tsx
+++ app/components/profile/education-list.tsx
@@ -9,7 +9,7 @@ export default function EducationList() {
           <div key={index}>
             <li className={`flex justify-between gap-x-4 py-3 ${index === siteConfig.education.length - 1 ? '' : 'border-b'}`}>
               <div className="flex min-w-0 w-full space-x-4 justify-center items-center">
-                <img className="h-14 w-14 ring-1 ml-1 ring-slate-200 flex-none rounded-full bg-gray-50" src={item.imageUrl} alt="" />
+                <img className="h-14 w-14 ring-1 ml-1 ring-slate-200 flex-none rounded-full bg-gray-50" src={item.imageURL} alt="" />
                 <div className="flex flex-col w-full ">
                   <div className="flex flex-col md:flex-row md:space-x-3 justify-between">
                     <p className="text-sm md:text-sm font-semibold text-left dark:text-slate-300">{item.organization}</p>
